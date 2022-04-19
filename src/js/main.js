cc6b1ad5471476c3f871406ecf80c4faf7e1b640
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-04-19 14:42:14 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,33 @@
 import { burgermenu } from "./menu"
-// import { gsap } from "gsap"
-// import { Draggable } from "gsap/Draggable"
+import { gsap } from "gsap"
+import { Draggable } from "gsap/Draggable"
 
-// gsap.registerPlugin(Draggable)
+gsap.registerPlugin(Draggable)
 
 document.addEventListener("DOMContentLoaded", burgermenu())
 
 const cat = document.querySelector("#btn-cat")
 
-// Draggable.create(cat, {
-//   type: "y",
-//   edgeResistance: 0.15,
-//   bounds: ".intro--wrapper",
-//   trigger: cat,
-//   onDragEnd: (e) => {
-//     let yPos = e.pageY
-//     let xPos = e.pageX
-//     console.log("xpos:" + xPos + ", ypos:" + yPos)
-//     // if (yPos < 60) {
-//     //   window.location.href = "works.html"
-//     // } else if (yPos > 600) {
-//     //   window.location.href = "about.html"
-//     // }
-//   },
-// })
+Draggable.create(cat, {
+  type: "y",
+  edgeResistance: 0.15,
+  bounds: ".intro--wrapper",
+  trigger: cat,
+  onDrag: function () {
+    var perc = parseInt((100 * (this.y - this.maxY)) / (this.minY - this.maxY)),
+      topPerc = perc,
+      bottomPerc = 100 - perc
+    console.log({ perc, topPerc, bottomPerc })
+  },
+
+  onDragEnd: (e) => {
+    let yPos = e.pageY
+    let xPos = e.pageX
+    console.log("xpos:" + xPos + ", ypos:" + yPos)
+    if (yPos < 60) {
+      window.location.href = "../works.html"
+    } else if (yPos > 600) {
+      window.location.href = "../about.html"
+    }
+  },
+})
